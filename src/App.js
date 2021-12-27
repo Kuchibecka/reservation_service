642b79968c6a44b402453818df0235bdc4624568
IgorKurylev <kurilevigor@mail.ru>
2021-12-27 14:29:05 +0300

    add: login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HomeComponent from "./component/HomeComponent"
+import HomeComponent from "./component/HomeComponent";
+import Login from "./component/Login";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomeComponent/>}/>
+                <Route path="/" element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     );
